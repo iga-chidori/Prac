b1b3f1945c95571d82272fcf328afee206e2b3e9
iga-chidori <ucfnhch@ucl.ac.uk>
2021-10-27 13:42:07 +0100

    first commit

---
+++ Geographic library in R.R
@@ -0,0 +1,37 @@
+library(sf) 
+# classes and functions of vector data
+library(terra)
+# classes and functions of raster data
+library(spData)
+# load geographic data
+library(spDataLarge)
+# load longer geographic data
+library(RSQLite)
+# Load SQL language in R
+library(rgdal)
+# Bindings for the 'Geospatial' Data Abstraction Library
+library(tidyverse)
+# The tidyverse is an opinionated collection of R packages designed for data science.
+library(dplyr)
+# A Grammar of Data Manipulation, which has multiple verbs that allow you to change your data into a suitable format
+# library(here)
+# Library that helps to find your files
+library(janitor)
+# janitor has simple little tools for examining and cleaning dirty data.
+library(plotly)
+# create interactive web graphics via plotly
+library(maptools)
+library(tmaptools)
+library(tmap)
+library(classInt)
+library(RColorBrewer)
+library(rgeos)
+library(downloader)
+library(geojsonio)
+library(OpenStreetMap)
+library(here)
+library(raster)
+library(fs)
+library(rmapshaper)
+library(usethis)
+
